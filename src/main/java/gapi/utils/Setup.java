ba2a3f67ad4e37d3b1f4629bb0277e8a87694c54
Fernando Ultremare <feroult@gmail.com>
2014-07-28 16:02:27 -0300

    change order of properties and env variables priority

--- src/main/java/gapi/utils/Setup.java
+++ src/main/java/gapi/utils/Setup.java
@@ -4,10 +4,10 @@ import java.io.File;
 import java.net.URL;
 
 public class Setup {
-	
+
 	private static final String GAPI_SERVICE_ACCOUNT_EMAIL = "gapi.service.account.email";
 	private static final String GAPI_SERVICE_ACCOUNT_KEY = "gapi.service.account.key";
-	
+
 	private static final String GAPI_SERVICE_ACCOUNT_EMAIL_ENV = "GAPI_SERVICE_ACCOUNT_EMAIL";
 	private static final String GAPI_SERVICE_ACCOUNT_KEY_ENV = "GAPI_SERVICE_ACCOUNT_KEY";
 
@@ -18,14 +18,14 @@ public class Setup {
 	private static String getServiceAccountKey() {
 		return getProperty(GAPI_SERVICE_ACCOUNT_KEY, GAPI_SERVICE_ACCOUNT_KEY_ENV);
 	}
-	
+
 	private static String getProperty(String propertyKey, String environmentKey) {
-		String property = System.getProperty(propertyKey);
-		
-		if(property == null) {
-			return System.getenv(environmentKey);
+		String property = System.getenv(environmentKey);
+
+		if (property == null) {
+			return System.getProperty(propertyKey);
 		}
-		
+
 		return property;
 	}
 
@@ -36,5 +36,5 @@ public class Setup {
 		}
 
 		return new File(getServiceAccountKey());
-	}	
+	}
 }
