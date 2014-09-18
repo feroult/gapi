f89eadb8b977250e65a0d7a2a94c870e3834c346
Rodrigo S Magalhaes <rsilvamagalhaes@gmail.com>
2014-09-18 09:30:43 -0300

    Google groups GAE

--- src/main/java/gapi/GoogleAPI.java
+++ src/main/java/gapi/GoogleAPI.java
@@ -1,5 +1,6 @@
 package gapi;
 
+import gapi.utils.EnvironmentUtil;
 import gapi.utils.Setup;
 
 import java.io.IOException;
@@ -8,64 +9,50 @@ import java.security.GeneralSecurityException;
 import java.util.Arrays;
 import java.util.List;
 
+import com.google.api.client.auth.oauth2.BearerToken;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.extensions.appengine.auth.oauth2.AppIdentityCredential;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.admin.directory.Directory;
 import com.google.api.services.drive.Drive;
+import com.google.appengine.api.appidentity.AppIdentityService;
+import com.google.appengine.api.appidentity.AppIdentityService.GetAccessTokenResult;
 import com.google.gdata.client.spreadsheet.SpreadsheetService;
 
 public class GoogleAPI {
-
 	private Credential credential;
-
 	private DriveAPI drive;
-
 	private SpreadsheetAPI spreadsheet;
-	
 	private GoogleGroupAPI group;
 
 	public GoogleAPI() {
 		try {
-			credential = new GoogleCredential.Builder()
-							.setTransport(getTransport())
-							.setJsonFactory(getJsonFactory())
-							.setServiceAccountId(Setup.getServiceAccountEmail())
-							.setServiceAccountScopes(
-							Arrays.asList("https://spreadsheets.google.com/feeds", "https://docs.google.com/feeds"))							
-							.setServiceAccountPrivateKeyFromP12File(Setup.getServiceAccountKeyFile()).build();
+			List<String> accountscopes = Arrays.asList("https://spreadsheets.google.com/feeds", 
+													   "https://docs.google.com/feeds");
+			if (EnvironmentUtil.isGae()) {
+				credential = generateCredentialForGae(accountscopes);
+			} else {
+				credential = generateCredentialWithP12(accountscopes);
+			}
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
 	}
-	
+
 	public GoogleAPI(List<String> accountscopes) {
 		try {
-			credential = new GoogleCredential.Builder()
-							.setTransport(getTransport())
-							.setJsonFactory(getJsonFactory())
-							.setServiceAccountId(Setup.getServiceAccountEmail())
-							.setServiceAccountScopes(accountscopes)
-							.setServiceAccountUser(Setup.getServiceAccountUser())
-							.setServiceAccountPrivateKeyFromP12File(Setup.getServiceAccountKeyFile()).build();
+			if (EnvironmentUtil.isGae()) {
+				credential = generateCredentialForGae(accountscopes);
+			} else {
+				credential = generateCredentialWithP12(accountscopes);
+			}
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
-	}
-
-	private HttpTransport getTransport() {
-		return new NetHttpTransport();
-	}
-
-	private JsonFactory getJsonFactory() {
-		return new GsonFactory();
-	}
-
-	private Drive driveService() {
-		return new Drive.Builder(getTransport(), getJsonFactory(), credential).setApplicationName("gapi").build();
 	}
 
 	public SpreadsheetService spreadsheetService() {
@@ -95,7 +82,39 @@ public class GoogleAPI {
 		return group;
 	}
 	
+	private GoogleCredential generateCredentialWithP12(List<String> accountscopes) throws GeneralSecurityException, IOException {
+		return new GoogleCredential.Builder()
+					.setTransport(getTransport())
+					.setJsonFactory(getJsonFactory())
+					.setServiceAccountId(Setup.getServiceAccountEmail())
+					.setServiceAccountScopes(accountscopes)
+					.setServiceAccountUser(Setup.getServiceAccountUser())
+					.setServiceAccountPrivateKeyFromP12File(Setup.getServiceAccountKeyFile()).build();
+	}
+	
+	private Credential generateCredentialForGae(List<String> accountscopes) {
+		AppIdentityCredential appIdentityCredential = new AppIdentityCredential(accountscopes);
+		AppIdentityService appIdentityService = appIdentityCredential.getAppIdentityService();
+		GetAccessTokenResult accessToken = appIdentityService.getAccessToken(accountscopes);
+		Credential cred = new Credential(BearerToken.authorizationHeaderAccessMethod());
+		cred.setAccessToken(accessToken.getAccessToken());
+		return cred;
+	}
+	
+	private HttpTransport getTransport() {
+		return new NetHttpTransport();
+	}
+
+	private JsonFactory getJsonFactory() {
+		return new GsonFactory();
+	}
+
+	private Drive driveService() {
+		return new Drive.Builder(getTransport(), getJsonFactory(), credential).setApplicationName("gapi").build();
+	}
+
 	private Directory directoryService() throws GeneralSecurityException, IOException, URISyntaxException {
 		return new Directory.Builder(getTransport(), getJsonFactory(), credential).setApplicationName("gapi").build();
 	}
+	
 }
